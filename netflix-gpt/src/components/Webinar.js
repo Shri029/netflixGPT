31fedb1c9b5db922dcafc6dd5b46391e0506bc75
Shivani Tiwary <30630384+Shri029@users.noreply.github.com>
2025-01-19 18:24:58 +0530

    Added details

--- netflix-gpt/src/components/Webinar.js
+++ netflix-gpt/src/components/Webinar.js
@@ -16,6 +16,12 @@ import OM from '../assets/Om.png';
 import Smile from '../assets/Smile.png';
 import Yoga from '../assets/Yoga.png';
 import Meditate from '../assets/Meditate.png';
+import Meditate1 from '../assets/Meditate1.png';
+import Lotus from '../assets/Lotus.png';
+import Empowerment from '../assets/Empowerment.png';
+import Knowledge from '../assets/Knowledge.png';
+import Practise from '../assets/Practise.png';
+import Universal from '../assets/Universal.png';
 
 
 const Webinar = () => {
@@ -70,6 +76,39 @@ const Webinar = () => {
       },
     ]
 
+    const dataBenefit = [
+      {
+        "img": Meditate1,
+        "title": "Immediate Peace and Clarity ",
+        "content": "Experience a calm and focused mind after just one session.",
+      },
+      {
+        "img": Lotus,
+        "title": "Energized Chakras",
+        "content": "Learn to balance and harmonize your body’s energy centers for overall well-being.",
+      },
+      {
+        "img": Empowerment,
+        "title": "Personal Empowerment",
+        "content": "Unlock your hidden potential to overcome challenges with renewed strength and positivity.",
+      },
+      {
+        "img": Knowledge,
+        "title": "Life-Changing Spiritual Insight",
+        "content": "Gain access to ancient knowledge that has transformed lives for centuries.",
+      },
+      {
+        "img": Practise,
+        "title": "Tools for Daily Practice",
+        "content": "Learn techniques that you can easily incorporate into your routine for ongoing growth.",
+      },
+      {
+        "img": Universal,
+        "title": "Universal Connection",
+        "content": "Align with the universe’s energy to manifest abundance, health, and happiness in your life.",
+      },
+    ]
+
     const Details = ()=> {
       return <>
         <div className='w-5/12 bg-white rounded-lg mx-auto flex-wrap items-center text-black'>
@@ -81,11 +120,11 @@ const Webinar = () => {
         <h4 className='bg-orange-600 font-sans p-4 text-xl text-center font-semibold text-white'>Swami Rudraksha Dev</h4>
         <div className='flex-row p-2 m-2'>
           <div className='flex text-sm font-bold'>
-            <div className='flex border-orange-600  border-r-2 p-1'>
+            <div className='flex border-orange-600 border-r-2 p-1'>
               <img className='w-10 h-10' alt="calender"src={Calender}/>
               <div className='flex-row px-3'>
                 <h5 className='mb-1'>24th January, 2025 </h5>
-                <h5>7:00pm to 9:00pm</h5>
+                <h5>7:00pm to 9:00pm</h5>Unlock
               </div>
             </div>
             <img className='px-3'alt="zoom" src={Zoom}/>
@@ -125,6 +164,7 @@ const Webinar = () => {
 
     const Timer = () => {
       return <>
+      <div className='flex-row'>
           <h1 className='text-white font-bold text-3xl text-center mt-6'>Offer ends in</h1>
           <div className='flex flex-wrap items-center p-2 mx-auto justify-center'>
             {timer.map((data, key)=>(
@@ -134,29 +174,54 @@ const Webinar = () => {
               </div>
             ))}
           </div>
+          </div>
       </>
     }
 
     const Learning = () => {
       return <>
-        <h1 className='text-orange-500 font-bold text-3xl text-center my-10'>What you’ll learn in this webinar</h1>
-        <div className='w-[70%] mx-auto flex justify-center'>
+      <div className='flex-row my-10'>
+        <h1 className='text-orange-500 font-bold text-3xl text-center'>What you’ll learn in this webinar</h1>
+        <div className='w-[80%] mt-5 mx-auto flex justify-center'>
           {dataYoga.map((data, key)=>(
-            <div key={key} className='m-2 pb-3 text-center font-sans bg-white rounded-lg'>
-              <img className='w-56' src={data.img}/>
-              <h5 className='text-md font-bold'>{data.title}</h5>
-              <p1 className='m-3 text-sm font-semibold'>{data.content}</p1>
+            <div key={key} className='m-5 text-center font-sans bg-white rounded-lg'>
+              <img className='w-60' src={data.img}/>
+              <h5 className='text-md font-bold py-1'>{data.title}</h5>
+              <p1 className='text-sm font-semibold'>{data.content}</p1>
             </div>
           ))}
         </div>
         <span className="flex items-center justify-center">
         <button className="py-4 px-12 bg-orange-500 rounded-lg text-sm text-white font-semibold" onClick={handleButtonClick}>Register Now</button>
         </span>
+        </div>
+      </>
+    }
+
+    const Attendee = () => {
+      return <>
+              <div className='w-[55%] flex mx-auto mt-[28rem] justify-center'>
+                <img className='bg-black' alt="background" src={Background}/>
+                <div className='absolute my-5 bg-opacity-60'>
+                  <span><h1 className='text-white font-bold text-3xl text-center my-5'>Who Should Attend?</h1></span>
+                  <div className='w-[60%] mx-auto my-5 flex flex-wrap justify-between'>
+                    {dataAttendee.map((data, key)=>(
+                      <div key={key} className='min-w-56 m-5 w-20 text-center font-sans rounded-lg'>
+                        <div className='bg-orange-600 rounded-lg w-14 h-14 items-center justify-center mx-auto'><img className='w-9' src={data.img}/></div>
+                        <p1 className='text-sm font-semibold text-white'>{data.content}</p1>
+                      </div>
+                    ))}
+                  </div>
+                  <span className="flex items-center justify-center">
+                    <button className="my-5 py-4 px-12 bg-orange-500 rounded-lg text-sm text-white font-semibold" onClick={handleButtonClick}>Free Registeration</button>
+                  </span>
+                </div>
+              </div>
       </>
     }
   return (
     <div className='flex flex-row flex-wrap'>
-      <div className='container justify-between items-center'>
+      <div className='container flex-row justify-between items-center'>
         <div className='flex flex-wrap'>
           <img className='bg-black w-full h-[850]' alt="background" src={Background}/>
             <div className='w-8/12 absolute mx-auto my-10 right-0 left-0 flex-wrap flex-row justify-between items-center'>
@@ -170,25 +235,28 @@ const Webinar = () => {
               </div>
               <Timer/>
               <Learning/>
-              </div> 
-              <div className='flex mx-auto mt-[25rem] items-center justify-center'>
-                <img className='bg-black w-[70%]' alt="background" src={Background}/>
-                <div className='w-8/12 absolute mx-auto flex-row justify-between items-center my-1 bg-opacity-70'>
-                  <span><h1 className='text-white font-bold text-3xl text-center my-5'>Who Should Attend?</h1></span>
-                  <div className='w-[100%] mx-auto flex'>
-                    {dataAttendee.map((data, key)=>(
-                      <div key={key} className='m-5 pb-3 w-20 text-center font-sans rounded-lg bg-yellow-300 min-w-200'>
-                        <div className='bg-orange-600 rounded-lg w-14 h-14'><img className='w-9 mx-auto my-auto' src={data.img}/></div>
-                        <p1 className='text-sm font-semibold text-white'>{data.content}</p1>
-                      </div>
-                    ))}
+            </div> 
+            <Attendee/>
+            
+        <div className='w-[55%] my-12 mx-auto flex-row flex-wrap'>
+          <h1 className='text-orange-500 font-bold text-3xl text-center'>What you’ll learn in this webinar</h1>
+            <div className='flex flex-wrap flex-row my-5 mx-auto text-black'>
+              {dataBenefit.map((data, key)=>(
+                <div key={key} className='min-w-[25rem] m-2 p-1 w-20 flex-row flex-wrap font-sans rounded-md text-black border border-solid-1 border-orange-600  items-center'>
+                  <div className='w-14 h-14 bg-orange-300 rounded-lg'><img className='w-9' src={data.img}/></div>
+                  <div className='w-[30%] bg-orange-400'>
+                    <h5 className='text-md font-bold'>{data.title}</h5>
+                    <p1 className='text-sm font-semibold'>{data.content}</p1>
                   </div>
-                  <span className="flex items-center justify-center">
-                    <button className="my-10 py-4 px-12 bg-orange-500 rounded-lg text-sm text-white font-semibold" onClick={handleButtonClick}>Free Registeration</button>
-                  </span>
                 </div>
-              </div>
+              ))}
+            </div>
+            <span className="flex items-center justify-center">
+              <button className="px-5 py-3 bg-orange-500 rounded-lg text-md text-white font-semibold" onClick={handleButtonClick}>Click here to register</button>
+            </span>
           </div>
+        </div>
+
       </div>
     </div>
   )
